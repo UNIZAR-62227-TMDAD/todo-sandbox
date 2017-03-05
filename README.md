0bf4898d7d610a70dec31d7b7bc4bad04f7a049d
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2017-03-05 16:35:23 +0100

    Upgrade to 1.5.2, fix and clean

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # todo-sandbox
 
-Template for developing RESTful Web Services
+Template for developing RESTful Web Services with Swagger enabled. 
+
+Run with `gradle bootRun` and visit [http://localhost:8080/swagger-ui.html](http://localhost:8080/swagger-ui.html)
+
--- src/main/java/tarea/Aplicacion.java
+++ src/main/java/tarea/Aplicacion.java
@@ -3,14 +3,11 @@ package tarea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Aplicacion {
-
 
     public static void main(String[] args) {
         SpringApplication.run(Aplicacion.class, args);
     }
-
 
 }
